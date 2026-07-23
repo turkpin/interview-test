85f1729f43672b1ac5744e92635fd242a072d3a6
Oktay Aydoğan <oktay@turkpin.com>
2026-07-23 21:08:40 +0300

    (feat) deps: dotenv

--- index.php
+++ index.php
@@ -3,5 +3,9 @@ session_start();
 require_once 'vendor/autoload.php';
 require_once 'src/classes/Main.php';
 
+if (file_exists(__DIR__ . '/.env')) {
+    Dotenv\Dotenv::createImmutable(__DIR__)->safeLoad();
+}
+
 $main = new Main();
 $main->run();
